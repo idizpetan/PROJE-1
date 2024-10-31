5d0c68e9b4be2d08ba9d77679290d6f15326023a
idizpetan <idizpetan@gmail.com>
2024-10-31 16:54:54 +0300

    draft

--- proje.1.js
+++ proje.1.js
@@ -8,17 +8,3 @@ form.addEventListener('submit', function(event) {
         event.preventDefault(); // Form gönderimini engelle
     }
 });
-document.getElementById("searchInput").addEventListener("keyup", function search() {
-    var input = document.getElementById("searchInput").value.toLowerCase();
-    var cards = document.getElementsByClassName("card");
-    window.location.href =`Teacher.html`
-
-    for (var i = 0; i < cards.length; i++) {
-        var cardText = cards[i].innerText.toLowerCase();
-        if (cardText.includes(input)) {
-            cards[i].parentElement.style.display = "block"; // Kartı göster
-        } else {
-            cards[i].parentElement.style.display = "none"; // Kartı gizle
-        }
-    }
-});
